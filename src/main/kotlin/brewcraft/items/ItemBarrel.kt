50df1d1aff3b29c002bf2c6da6b52265c64a7ec8
Sohei Yamaga <me@eliza.link>
2019-03-10 01:22:17 +0900

    樽にアイテムを入れられるようにした

--- src/main/kotlin/brewcraft/items/ItemBarrel.kt
+++ src/main/kotlin/brewcraft/items/ItemBarrel.kt
@@ -5,8 +5,10 @@ import brewcraft.util.RegisterableItem
 import brewcraft.util.RegisterableModel
 import brewcraft.BrewCraft
 import brewcraft.items.utils.BlockContainerBase
+import net.minecraft.block.state.IBlockState
 import net.minecraft.entity.player.EntityPlayer
 import net.minecraft.item.Item
+import net.minecraft.item.ItemStack
 import net.minecraft.nbt.NBTTagCompound
 import net.minecraft.tileentity.TileEntity
 import net.minecraft.util.ResourceLocation
@@ -17,6 +19,8 @@ import net.minecraftforge.fml.common.SidedProxy
 import net.minecraftforge.fml.common.registry.GameRegistry
 import net.minecraft.network.play.server.SPacketUpdateTileEntity
 import net.minecraft.network.NetworkManager
+import net.minecraft.util.EnumFacing
+import net.minecraft.util.EnumHand
 import java.lang.Exception
 
 object Barrel: RegisterableItem, RegisterableModel {
@@ -39,13 +43,31 @@ object Barrel: RegisterableItem, RegisterableModel {
 object BlockBarrel: BlockContainerBase(
         itemID = Barrel.itemID
 ) {
-    override fun onBlockClicked(world: World, pos: BlockPos, state: EntityPlayer) {
+    override fun onBlockActivated(world: World, pos: BlockPos, state: IBlockState, player: EntityPlayer, hand: EnumHand, facing: EnumFacing, x: Float, y: Float, z: Float): Boolean {
         val tile: TileEntity? = world.getTileEntity(pos)
-        if (tile is TileBarrel && !world.isRemote) {
-          tile.markDirty()
+        val heldItem: Item = player.getHeldItem(EnumHand.MAIN_HAND).item
+        if (tile is TileBarrel && !world.isRemote && tile.container.canIntoItem(heldItem)) {
+            tile.container.itemIntoBarrel(heldItem)
+            declPlayerInventryCurrenyItem(player)
+            tile.markDirty()
+
+            for (i in tile.container.items.toList()) {
+                println("${i.first.unlocalizedName} - ${i.second}")
+            }
         }
-        super.onBlockClicked(world, pos, state)
+        return super.onBlockActivated(world, pos, state, player, hand, facing, x, y, z)
     }
+
+    fun declPlayerInventryCurrenyItem(player: EntityPlayer) {
+        val heldItemStack: ItemStack = player.getHeldItem(EnumHand.MAIN_HAND)
+        // プレイヤーの手持ちを減少
+        heldItemStack.count--
+        if (!player.isCreative && heldItemStack.count <= 0) {
+            player.inventory.setInventorySlotContents(player.inventory.currentItem, ItemStack.EMPTY)
+        }
+        player.inventory.markDirty()
+    }
+
     override fun createNewTileEntity(p0: World, p1: Int): TileEntity? {
         return TileBarrel()
     }
@@ -57,7 +79,7 @@ object ItemBlockBarrel: ItemBlockBase(
 )
 
 class TileBarrel: TileEntity() {
-    private val storedContainer: BarrelContainer = BarrelContainer()
+    val container: BarrelContainer = BarrelContainer()
 
     override fun readFromNBT(tagCompound: NBTTagCompound) {
         super.readFromNBT(tagCompound)
@@ -104,23 +126,60 @@ class ClientProxyBarrel: CommonProxyBarrel() {
 }
 
 data class BarrelContainer(
-        val items: MutableMap<Item, Int> = mutableMapOf()
+        val items: MutableMap<Item, Int> = mutableMapOf(),
+        private var internalLiquidCapacity: Int = 0,
+        private val maxInternalLiquidCapacity: Int = 1,
+        private var internalItemCapacity: Int = 0,
+        private val maxInternalItemCapacity: Int = 32
 ) {
+    fun canIntoItem(item: Item): Boolean {
+        return canIntoItemVearias(item) && canIntoItemCapacity(item)
+    }
+
+    private fun canIntoItemVearias(item: Item): Boolean {
+        val list: List<Item> = listOf(
+                Item.getByNameOrId("minecraft:wheat")!!,
+                Item.getByNameOrId("minecraft:water_bucket")!!,
+                Item.getByNameOrId("brewcraft:new_make_spirit")!!
+        )
+        return list.contains(item)
+    }
+    private fun canIntoItemCapacity(item: Item): Boolean {
+        if (isLiquid(item)) {
+            return internalLiquidCapacity < maxInternalLiquidCapacity
+        } else {
+            return internalItemCapacity < maxInternalItemCapacity
+        }
+    }
+    // TODO: Liquid判定をItemに付けられるようにする, LiquidInterfaceかなにか？
+    fun isLiquid(item: Item): Boolean {
+         val list: List<Item> = listOf(
+                Item.getByNameOrId("minecraft:water_bucket")!!,
+                Item.getByNameOrId("brewcraft:new_make_spirit")!!
+        )
+        return list.contains(item)
+    }
     fun itemIntoBarrel(item: Item) {
         items[item] = 1 + (items[item] ?: 0)
+        if (isLiquid(item)) { internalLiquidCapacity++ } else { internalItemCapacity++ }
     }
 }
 
+data class BarrelMaterial(
+       val neccesaryTick: Int,
+       val material: Map<Item, IntRange>
+)
+
 object BarrelRecipe {
-    private val recipes: MutableMap<Item, Map<Item, ClosedRange<Int>>> = mutableMapOf()
+    private val recipes: MutableMap<Item, BarrelMaterial> = mutableMapOf()
 
     // TODO: 時間も設定できるように, 樽の材質で結果を変更させても良い
-    fun register(result: Item, vararg recipe: Pair<Item, ClosedRange<Int>>) {
+    fun register(result: Item, neccesaryTick: Int, vararg newRecipe: Pair<Item, IntRange>) {
         // すでに同じレシピが登録されていた場合例外
-        if (recipes[result] == recipe.toMap()) {
+        if (recipes[result]?.material == newRecipe) {
             throw Exception("This recipe is already registered.")
         }
-        recipes[result] = recipe.toMap()
+        recipes[result] = BarrelMaterial(neccesaryTick, newRecipe.toMap())
     }
 
     fun matchOf(barrelContainer: BarrelContainer): Item? {
@@ -128,7 +187,7 @@ object BarrelRecipe {
         val barrelItems: List<Pair<Item, Int>> = barrelContainer.items.toList()
         recipes.forEach { recipe ->
             val result: Item = recipe.key
-            val recipe: Map<Item, ClosedRange<Int>> = recipe.value
+            val recipe: Map<Item, IntRange> = recipe.value.material
             if (recipe.size == barrelItems.size) {
                 if (barrelItems.all { item ->
                     recipe[item.first] ?. contains(item.second) ?: false
--- src/main/kotlin/brewcraft/recipe/BarrelRecipes.kt
+++ src/main/kotlin/brewcraft/recipe/BarrelRecipes.kt
@@ -1,12 +1,16 @@
 package brewcraft.recipe
 
 import brewcraft.items.BarrelRecipe
+import brewcraft.items.ItemNewMakeSpirit
 import brewcraft.items.ItemWhiskey
 import brewcraft.util.RegisterableItem
 
-
 object BarrelRecipes: RegisterableItem {
     override fun registerItem() {
-        // BarrelRecipe.register()
+        BarrelRecipe.register(
+                ItemWhiskey,
+                24000, // 1day
+                ItemNewMakeSpirit to IntRange(1,1)
+        )
     }
 }
